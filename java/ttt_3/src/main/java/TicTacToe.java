2803f4ced04ab6839a31e21bec32440395a05a90
alee <alee@alee-laptop>
2018-12-07 13:27:37 -0800

    tictactoe board datastructure

--- java/ttt_3/src/main/java/TicTacToe.java
+++ java/ttt_3/src/main/java/TicTacToe.java
@@ -1,6 +1,11 @@
 public class TicTacToe {
+	private int[][] board;
 
     public static void main(String[] args) {
         System.out.println("Hellooo World!");
+    }
+    
+    public TicTacToe() {
+    	board = new int[3][3];
     }
 }
