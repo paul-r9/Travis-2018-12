f4b0c1ade86fe74567ba7942fb6e18df67cc3499
Paul Liebrand <paul.liebrand@pacificlife.com>
2018-12-07 13:47:37 -0800

    Row 1 Column 1 Added and verified is empty

--- csharp/ttt_4/TicTacToeTests/TickTacToeBoard.cs
+++ csharp/ttt_4/TicTacToeTests/TickTacToeBoard.cs
@@ -8,10 +8,12 @@ namespace TravisTests
         public TickTacToeBoard()
         {
             Players = new List<Player>();
+            Row1Col1 = "";
         }
 
         public int NumberOfPlayers { get { return Players.Count; }}
         public List<Player> Players { get; internal set; }
+        public string Row1Col1 { get; internal set; }
 
         internal void AddPlayer(Player player)
         {
