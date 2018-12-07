dbce537479c83df3daf64f134b0487640f51be44
Matt Brunmeier <matt@velexo.com>
2018-12-07 13:45:01 -0800

    Refactored new board

--- java/ttt_7/src/test/java/SampleTest.java
+++ java/ttt_7/src/test/java/SampleTest.java
@@ -10,21 +10,21 @@ public class SampleTest {
     }
 
     @Test
-    public void NewBoard_boardIsEmpty() {
-        // arrange
-        TicTacToe ttt = new TicTacToe();
+    public void NewBoard_boardIsEmptyAfterReset() {
         char [][] board = new char [3][3];
         for (int outerIndex = 0; outerIndex < 3; outerIndex++ ){
             for (int innerIndex = 0; innerIndex < 3; innerIndex++ ){
                 board [outerIndex][innerIndex] = ' ';
             }
         }
-        // act on ttt
+
+        // arrange
+        TicTacToe ttt = new TicTacToe();
 
         // assert
         Assert.assertArrayEquals(board,ttt.getBoard());
     }
-
+    
     //    A B C
     // 1. _ X _
     // 2. _ _ _
